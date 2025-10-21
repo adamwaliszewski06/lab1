278960a5dad1ddf96d6e77b1eb778a532d6086e3
adamwaliszewski06 <adam.waliszewski@gmail.com>
2025-10-21 14:48:13 +0200

    zad 2 i 3

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,20 +1,35 @@
-// lab1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+int main(){
+	/* zad2
+	cout <<"      *" << endl;
+	cout << "     ***" << endl;
+	cout << "    *****" << endl;
+	cout << "**************" << endl;
+	cout << "  ********** " << endl;
+	cout << "   ********"<< endl;
+	cout << "    **  ";
+	cout << "** "<< endl;
+	cout << "    * ";
+	cout << "   *" << endl;
+	*/
 
-int main()
-{
-    std::cout << "Hello World!\n";
+/*  zad3
+* 
+	cout << "LISTA OBECNOSCI" << endl;
+	cout <<"____________________________________" <<endl;
+	cout << "NR  ||    IMIE   ||     NAZWISKO  || " << endl;
+	cout << "1   ||TOMMY      || ATKINS        ||" << endl;
+	cout << "2   ||JOHN       || DOE           ||" << endl;
+	cout << "3   ||JAN        || KOWALSKI      ||" << endl;
+*/
+
+
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
