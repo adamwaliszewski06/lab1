f96ceba5dac39d7c4eab705b4f0b0cae9972d3ce
adamw06 <adam.waliszewski06@gmail.com>
2025-10-26 13:22:43 +0100

    26.10.2025

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,7 +1,17 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+
+	/*zad1
+	cout << "    X           X           X" << endl;
+	cout << "  X   X          X         X" << endl;
+	cout << " XXXXXXX          X   X   X" << endl;
+	cout << "X       X          X    X" << endl;
+	*/
+
+
+
 	/* zad2
 	cout <<"      *" << endl;
 	cout << "     ***" << endl;
@@ -15,21 +25,53 @@ int main(){
 	cout << "   *" << endl;
 	*/
 
-/*  zad3
-* 
-	cout << "LISTA OBECNOSCI" << endl;
-	cout <<"____________________________________" <<endl;
-	cout << "NR  ||    IMIE   ||     NAZWISKO  || " << endl;
-	cout << "1   ||TOMMY      || ATKINS        ||" << endl;
-	cout << "2   ||JOHN       || DOE           ||" << endl;
-	cout << "3   ||JAN        || KOWALSKI      ||" << endl;
-*/
 
+	/*  zad3
 
+		cout << "LISTA OBECNOSCI" << endl;
+		cout <<"____________________________________" <<endl;
+		cout << "NR  ||    IMIĘ   ||     NAZWISKO  || " << endl;
+		cout << "------------------------------------" << endl;
+		cout << "1   ||MAX        || MUSTERMANN    ||" << endl;
+		cout << "2   ||JOHN       || DOE           ||" << endl;
+		cout << "3   ||JAN        || KOWALSKI      ||" << endl;
+	*/
 
+	/* zad 4.1
+	int a;
+	int b;
+	cout << "Enter the first number:" << endl;
+	cin >> a;
+	cout << "Enter the second number:" << endl;
+	cin >> b;
+	int c = a * b;
+	cout << "Sum: ";
+	cout << a + b << endl;
+	cout << "Product: ";
+	cout << c << endl;
+	*/
+
+	/* zad 4.2
+	float a;
+	float b;
+	cout << "Enter the first number:" << endl;
+	cin >> a;
+	cout << "Enter the second number:" << endl;
+	cin >> b;
+	float c = a * b;
+	cout << "Sum: ";
+	cout << a + b << endl;
+	cout << "Product: ";
+	cout << c << endl;
+	*/
+
+	/*zad 5
+	int F, C;
+	cout << "Enter temperature in Fahrenheit: ";
+	cin >> F;
+	C = 5 * (F - 32) / 9;
+	cout << "Temperature in Celcius: ";
+	cout << C;
+	*/
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
